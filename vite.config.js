91998c0df6e8de12a3afaf66cb0d042073ef3582
shubhamdhiman <shubham.dhiman.mehra@gmail.com>
2026-02-12 22:50:09 +0530

    update-1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
    server: {
     proxy: {
-      '/api': {
+      '/api/chartdata': {
         target: 'http://localhost:3000',
         changeOrigin: true
       }
